d21421a832c87567ae905b7b30a43db6d889f87f
Haroon Ghafoori <hghafoori@beachbody.com>
2015-01-06 17:12:53 +0530

    Project skeleton

---
+++ xifire/target/.generated/com/google/gwt/lang/com_00046xebia_00046AppWidgetSet__EntryMethodHolder.java
@@ -0,0 +1,6 @@
+package com.google.gwt.lang;
+public class com_00046xebia_00046AppWidgetSet__EntryMethodHolder {
+  public static final void init() {
+    // Filled in by the compiler to call entry methods.
+  }
+}
---
+++ xifire/target/xifire-1.0-SNAPSHOT/VAADIN/widgetsets/com.xebia.AppWidgetSet/deferredjs/30E5AD4656EFA8F70235BE46ADB5D0E3/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_xebia_AppWidgetSet.runAsyncCallback2('U8(978,1,tkd);_.vc=function Y_b(){mRb((!fRb&&(fRb=new rRb),fRb),this.b.e)};Yed(Vh)(2);\n//# sourceURL=com.xebia.AppWidgetSet-2.js\n')
