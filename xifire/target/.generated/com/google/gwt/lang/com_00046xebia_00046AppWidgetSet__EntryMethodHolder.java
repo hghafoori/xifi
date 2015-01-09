acac60579132f8cac79b3725524f6979ff9e032b
Chirag Goel <cgoel@xebia.com>
2015-01-09 16:13:35 +0530

    Created portlet project instead of VAADIN

--- xifire/target/.generated/com/google/gwt/lang/com_00046xebia_00046AppWidgetSet__EntryMethodHolder.java
+++
@@ -1,6 +0,0 @@
-package com.google.gwt.lang;
-public class com_00046xebia_00046AppWidgetSet__EntryMethodHolder {
-  public static final void init() {
-    // Filled in by the compiler to call entry methods.
-  }
-}
